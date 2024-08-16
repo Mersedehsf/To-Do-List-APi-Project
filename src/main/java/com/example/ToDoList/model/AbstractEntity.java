9eaf6bdcc39924eae35ecc0f973e5c496a715199
mersedeh <MersedehSadeghifard@yahoo.com>
2024-08-16 11:38:40 +0330

    first commit

---
+++ src/main/java/com/example/ToDoList/model/AbstractEntity.java
@@ -0,0 +1,15 @@
+package com.example.ToDoList.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AbstractEntity {
+
+    private Integer id;
+
+}
